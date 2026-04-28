b32b1566d712f804741e0bcb7c2a60aa04635325
mfwolffe <wolffemf@dukes.jmu.edu>
2026-04-28 18:14:27 -0400

    Fix version lookup to use renamed package name

--- src/dlm/__init__.py
+++ src/dlm/__init__.py
@@ -3,7 +3,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("dlm")
+    __version__ = version("document-language-model")
 except PackageNotFoundError:
     __version__ = "0.0.0+unknown"
 
